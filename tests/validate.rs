602346e8446c3d78e80fc5bb8daa1a9ee1056ddc
Valerian Saliou <valerian@valeriansaliou.name>
2020-12-10 09:22:28 +0100

    Fix code style (cargo fmt)
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- tests/validate.rs
+++ tests/validate.rs
@@ -3,27 +3,19 @@ extern crate card_validate;
 use card_validate::{Validate, ValidateError};
 
 fn visaelectron_numbers_valid() -> Vec<&'static str> {
-    vec![
-        "4917300800000000"
-    ]
+    vec!["4917300800000000"]
 }
 
 fn maestro_numbers_valid() -> Vec<&'static str> {
-    vec![
-        "6759649826438453"
-    ]
+    vec!["6759649826438453"]
 }
 
 fn forbrugsforeningen_numbers_valid() -> Vec<&'static str> {
-    vec![
-        "6007220000000004"
-    ]
+    vec!["6007220000000004"]
 }
 
 fn dankort_numbers_valid() -> Vec<&'static str> {
-    vec![
-        "5019717010103742"
-    ]
+    vec!["5019717010103742"]
 }
 
 fn visa_numbers_valid() -> Vec<&'static str> {
@@ -32,7 +24,7 @@ fn visa_numbers_valid() -> Vec<&'static str> {
         "4532983409238819",
         "4485600412608021",
         "4916252910064718",
-        "4916738103790259"
+        "4916738103790259",
     ]
 }
 
@@ -42,7 +34,7 @@ fn amex_numbers_valid() -> Vec<&'static str> {
         "377156543570043",
         "340173808718013",
         "375801706141502",
-        "372728319416034"
+        "372728319416034",
     ]
 }
 
@@ -52,7 +44,7 @@ fn mastercard_numbers_valid() -> Vec<&'static str> {
         "5431604665471808",
         "5571788302926264",
         "5411516521560216",
-        "5320524083396284"
+        "5320524083396284",
     ]
 }
 
@@ -62,15 +54,12 @@ fn discover_numbers_valid() -> Vec<&'static str> {
         "6011993225918523",
         "6011420510510997",
         "6011618637473995",
-        "6011786207277235"
+        "6011786207277235",
     ]
 }
 
 fn jcb_numbers_valid() -> Vec<&'static str> {
-    vec![
-        "3530111333300000",
-        "3566002020360505"
-    ]
+    vec!["3530111333300000", "3566002020360505"]
 }
 
 fn unionpay_numbers_valid() -> Vec<&'static str> {
@@ -78,7 +67,7 @@ fn unionpay_numbers_valid() -> Vec<&'static str> {
         "6271136264806203568",
         "6236265930072952775",
         "6204679475679144515",
-        "6216657720782466507"
+        "6216657720782466507",
     ]
 }
 
@@ -87,37 +76,24 @@ fn dinersclub_numbers_valid() -> Vec<&'static str> {
         "30569309025904",
         "38520000023237",
         "36700102000000",
-        "36148900647913"
+        "36148900647913",
     ]
 }
 
 fn gibberish_numbers_invalid() -> Vec<&'static str> {
-    vec![
-        "zduhehiudIHZHIUZHUI",
-        "0292DYYEFYFEFYEFEFIUH"
-    ]
+    vec!["zduhehiudIHZHIUZHUI", "0292DYYEFYFEFYEFEFIUH"]
 }
 
 fn unknown_numbers_invalid() -> Vec<&'static str> {
-    vec![
-        "00002837743671762",
-        "1136283774"
-    ]
+    vec!["00002837743671762", "1136283774"]
 }
 
 fn known_numbers_invalid() -> Vec<&'static str> {
-    vec![
-        "424242424",
-        "4242424242424244242424242",
-        "523631387710914"
-    ]
+    vec!["424242424", "4242424242424244242424242", "523631387710914"]
 }
 
 fn numbers_invalid_luhn() -> Vec<&'static str> {
-    vec![
-        "5236313877109141",
-        "6011420510510995"
-    ]
+    vec!["5236313877109141", "6011420510510995"]
 }
 
 fn valid_mixture() -> Vec<&'static str> {
@@ -156,28 +132,40 @@ fn valid_card() {
 #[test]
 fn gibberish_invalid() {
     for number in gibberish_numbers_invalid() {
-        assert_eq!(Validate::from(number) == Err(ValidateError::InvalidFormat), true);
+        assert_eq!(
+            Validate::from(number) == Err(ValidateError::InvalidFormat),
+            true
+        );
     }
 }
 
 #[test]
 fn unknown_invalid() {
     for number in unknown_numbers_invalid() {
-        assert_eq!(Validate::from(number) == Err(ValidateError::UnknownType), true);
+        assert_eq!(
+            Validate::from(number) == Err(ValidateError::UnknownType),
+            true
+        );
     }
 }
 
 #[test]
 fn known_invalid() {
     for number in known_numbers_invalid() {
-        assert_eq!(Validate::from(number) == Err(ValidateError::InvalidLength), true);
+        assert_eq!(
+            Validate::from(number) == Err(ValidateError::InvalidLength),
+            true
+        );
     }
 }
 
 #[test]
 fn invalid_luhn() {
     for number in numbers_invalid_luhn() {
-        assert_eq!(Validate::from(number) == Err(ValidateError::InvalidLuhn), true);
+        assert_eq!(
+            Validate::from(number) == Err(ValidateError::InvalidLuhn),
+            true
+        );
     }
 }
 
